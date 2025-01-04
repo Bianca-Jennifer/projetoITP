a8be288403384f3d50da606ec820d52a58145f69
Lucas Estanislau <lukinhasun@gmail.com>
2025-01-04 15:36:02 -0300

    conversor e codigo de barras juntado

--- main.c
+++ main.c
@@ -96,6 +96,8 @@ int main(int argc, char *argv[]) {
     int identificador = 0, espacamento_lateral = 0, quantidade_pixel = 0, altura = 0;
     char nome[20] = "";
     int cond;
+    char marcadorif[4] = {'1', '0', '1', '\0'};
+    char marcadorcentral[6] = {'0', '1', '0', '1', '0', '\0'};
 
     setlocale(0, "Portuguese");
 
@@ -126,6 +128,8 @@ int main(int argc, char *argv[]) {
                 snprintf(nome, sizeof(nome), "%s", argv[5]);
 
             }
+
+            //separador de digitos iniciais e digitos finais
             char esquerda[5], direita[5];
             char lcode[29], rcode[29];
                 strncpy(esquerda, argv[1], 4);
@@ -134,13 +138,28 @@ int main(int argc, char *argv[]) {
                 esquerda[4] = '\0'; 
                 direita[4] = '\0';
             
+            //converte os digitos iniciais em lcode e os finais em rcode
             conversor(esquerda, direita, lcode, rcode);
+
+            //junta marcadores digitos convertidos
+            char codigodebarras[68] = "";
+                strcat(codigodebarras, marcadorif);
+                strcat(codigodebarras, lcode);
+                strcat(codigodebarras, marcadorcentral);
+                strcat(codigodebarras, rcode);
+                strcat(codigodebarras, marcadorif);
+
+                codigodebarras[68] = '\0';
+
 
             printf("Informações: código = %d, espaçamento lateral = %d, quantidade de pixels = %d, altura = %d, nome do arquivo = %s\n", identificador, espacamento_lateral, quantidade_pixel, altura, nome);
             printf("%s\n", esquerda);
             printf("%s\n", direita);
             printf("%s\n", lcode);
             printf("%s\n", rcode);
+            printf("%s\n", codigodebarras);
+
+
         }
     }
 
